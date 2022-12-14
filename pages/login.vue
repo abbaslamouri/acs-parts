db22c3a8ac74bbf8309b3fbace91dd3e1f64143a
Abbas Lamouri <alamouri@GENVAC.local>
2022-12-14 08:20:25 -0500

    Not used

--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,19 @@
 <template>
   <div>
-    <div>SV={{ status }}</div>
-    <div>SV={{ data }}</div>
+    <!-- <div>SV={{ status }}</div> -->
+    <!-- <div>SV={{ data }}</div> -->
     <button @click="signinUser">Login</button>
     <button @click="signoutUser">Logout</button>
   </div>
 </template>
 
 <script setup lang="ts">
-const { status, data, signIn, signOut } = useSession()
-status.value // Session status: `unauthenticated`, `loading`, `authenticated`
-data.value // Session data, e.g., expiration, user.email, ...
-
 const signinUser = async () => {
-  await signIn('credentials', { username: 'jsmith', password: 'hunter2' })
+  // await signIn('credentials', { username: 'jsmith', password: 'hunter2' })
 }
 
 const signoutUser = async () => {
-  await signOut()
+  // await signOut()
 }
 </script>
 
