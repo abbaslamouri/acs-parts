411243103591d35ed4cb8461d96111a84481a245
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-12-19 07:16:48 -0500

    Working on fluid design

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,14 @@
 
     <button @click="signinUser">Login</button>
     <!-- <button @click="signupUser">Register</button> -->
-    <div>
+    <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
       type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
       remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
       Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
       of Lorem Ipsum.
-    </div>
+    </p>
     <img src="/home-page-hero-2500x800.jpg" alt="Home Page Hero" />
 
     <p>This is a narrow column of text, with a very long word: antidisestablishmentarianism.</p>
@@ -31,7 +31,7 @@ const signinUser = async () => {
 
 <style scoped>
 .main-div p {
-  width: 200px;
+  width: 1000px;
   border: 1px solid red;
 }
 /* h1 {
