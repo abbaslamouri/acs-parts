ff878e13a0a994d63123088ad6f506cb62a14e89
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-12-22 19:58:28 -0500

    Working on split Faux

---
+++ pages/split-faux-design.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts"></script>
+
+<template>
+  <article>
+    <div class="center-split">
+      <h1>
+        <span>Equal center split layout</span>
+      </h1>
+      <p>
+        <span>
+          Each item of <code>.center-split</code> is 50% wide. The content
+          within is sized with the <code>--center-split-max-width</code> Custom
+          Property. Inner content in all items is pushed into the center.
+        </span>
+      </p>
+    </div>
+    <div class="center-split">
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit
+          non mi porta gravida at eget metus. Etiam porta sem malesuada magna
+          mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.
+          Nullam id dolor id nibh ultricies vehicula ut id elit. Curabitur
+          blandit tempus porttitor. Maecenas sed diam eget risus varius blandit
+          sit amet non magna.
+        </span>
+      </p>
+      <h2>
+        <span>A large subheading</span>
+      </h2>
+    </div>
+    <div class="center-split">
+      <p>
+        <span>
+          Donec id elit non mi porta gravida at eget metus. Donec id elit non mi
+          porta gravida at eget metus. Aenean lacinia bibendum nulla sed
+          consectetur. Cras mattis consectetur purus sit amet fermentum. Nullam
+          id dolor id nibh ultricies vehicula ut id elit. Sed posuere
+          consectetur est at lobortis.
+        </span>
+      </p>
+      <p>
+        <span>
+          Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id
+          ligula porta felis euismod semper. Donec id elit non mi porta gravida
+          at eget metus. Maecenas sed diam eget risus varius blandit sit amet
+          non magna.
+        </span>
+      </p>
+    </div>
+  </article>
+</template>
+
+<style scoped></style>
